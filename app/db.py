8f8f4f224715763f2f892e1388265c868e13f8e0
steveviko <stevenviko@gmail.com>
2018-08-21 06:01:12 +0300

    Endpoint to get all qn[delivery #159913740]

--- app/db.py
+++ app/db.py
@@ -1,31 +1,14 @@
-users =[
-			{ 'username': 'venviko',
-		      'email': 'venviko@gmail.com',
-		      'password': '123'},
-			{
-              'username': 'steven', 
-			  'email': 'steven@gmail.com',
-			  'password': '1234' }
-    ]
-	
-questions = [
-            {
-                'qn_id': 1,
-                'qn_category': 'HTML',
-                'qn_title': 'what is Html?'    
-            },
-            {
-                'qn_id': 2,
-                'qn_category': 'TDD',
-                'qn_title': 'what is TDD?'
-            }]
-			
-answers = [
-			{'ans_id': 1,
-			 'qn_id': 1,
-			 'ans_content': 'HTML is a markup language.'
-			 },
-			{'ans_id': 2,
-			 'qn_id': 2,
-			 'ans_content': 'TDD is implementing code through testing upfront'
-			 }]
+# Create some test data for our collection in the form of a list of dictionaries.
+
+Users =[]
+questions=[ {'qn_id': 1,
+     'questions': 'What is HTML',
+     'username': 'Verno'
+     },
+    {'qn_id': 2,
+     'questions': 'What is TDD',
+     'username': 'steve'},
+    {'qn_id': 3,
+     'questions': 'What is python',
+     'username': 'musa'},]
+answers=[]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template,url_for,request, jsonify
+from app.db import Users,questions,answers
 
+# Create some test data for our catalog in the form of a list of dictionaries.
+users = [
+    {'id': 0,
+     'User FirstName': 'Moses',
+     'User LasnName': 'Verno',
+     'profession': 'Engineer.',
+     'Age': '39'},
+    {'id': 1,
+     'User FirstName': 'Mike',
+     'User LasnName': 'koola',
+     'profession': 'Software Developer.',
+     'Age': '16'},
+    {'id': 2,
+     'User FirstName': 'Chris',
+     'User LasnName': 'looki',
+     'profession': 'Doctor.',
+     'Age': '12'},
+]
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello world"
 
+@app.route('/api/v1/questions', methods=['GET'])
+def api_all_questions():
+    return jsonify(questions)
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
